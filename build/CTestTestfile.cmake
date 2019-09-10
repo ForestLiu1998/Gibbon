db3287748ce5ab5aa825ddd49a6c6b3434ee6916
byliu <2925666948@qq.com>
2019-09-10 14:50:28 +0800

    创建文件

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/byliu/Documents/Gibbon/src
+# Build directory: /home/byliu/Documents/Gibbon/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
